8a9fa45bb3fa90ccc1497cdd0e1a19f592ce1ff3
Timur Shafigullin <hometim55@gmail.com>
2023-09-25 18:32:05 +0900

    2.0.0-beta.11

--- FigmaGen.rb
+++ FigmaGen.rb
@@ -1,11 +1,11 @@
 class Figmagen < Formula
-  version '2.0.0-beta.10'
+  version '2.0.0-beta.11'
 
   desc 'A Swift command line tool to generate code for the Figma component library'
   homepage 'https://github.com/hhru/FigmaGen'
   head 'https://github.com/hhru/FigmaGen.git'
   url "https://github.com/hhru/FigmaGen/releases/download/#{version}/figmagen-#{version}.zip"
-  sha256 '51976a162a057978de58cdb9cf174b42b1077321766805cacfcffba4a7f1ae4d'
+  sha256 '68050c20dd3180c768d3e69b787bcaf96bee1104c8451c31d3f0b39fd7ffe8c2'
 
   def install
     system 'make', 'install_release', "PREFIX=#{prefix}"
